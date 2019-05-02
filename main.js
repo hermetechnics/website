2ca710143af2ece85ca9cd95efa7ee088c214cb4
jakubfiala <j4kub.fiala@gmail.com>
2019-05-02 21:48:08 +0100

    all the goodies

---
+++ main.js
@@ -0,0 +1,8 @@
+console.info(`
+HEY FELLOW CODE PERSON, WHO SO INTELLIGENTLY OPENED THEIR FAVOURITE BROWSER'S DEVTOOLS
+
+Here at Hermetechnics we're always looking for smart & talented folks like you.
+
+If you like solving hard problems and eating FREE PIZZA AND BEER <del>and working for free until we get funding for our blockchain-based social dogshit scanner app</del>
+drop us a line at hello@hermetechnics.life and put your favourite major arcanum in the subject ;)
+`);
